64288d84f5e209d98d8af67e584f05947c56f264
Matt Jones <jonesmr@gmail.com>
2019-04-30 00:37:18 +0000

    Printing current song for all groups

--- src/controller/main.rs
+++ src/controller/main.rs
@@ -13,13 +13,22 @@ struct SonosHouseholdsReply {
     pub households: Vec<SonosHousehold>,
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct SonosGroup {
+    pub id: String,
+    pub name: String,
+    pub coordinator_id: String,
+    pub playback_state: String, // Enum?
+    pub player_ids: Vec<String>,
+}
 
-    let access_token = std::env::var("ACCESS_TOKEN").expect("must set ACCESS_TOKEN");
-    let client_id = std::env::var("CLIENT_ID").expect("must set CLIENT)ID");
+#[derive(Deserialize)]
+struct SonosGroupsReply {
+    pub groups: Vec<SonosGroup>,
+}
 
-    let http_client = reqwest::Client::new();
+fn get_households(access_token: &str, client_id: &str, http_client: &reqwest::Client) -> SonosHouseholdsReply {
     let mut response = http_client
         .get("https://api.ws.sonos.com/control/api/v1/households")
         .bearer_auth(access_token)
@@ -32,9 +41,88 @@ fn main() {
 
     println!("{:?}",  response_body);
 
-    let reply: SonosHouseholdsReply = serde_json::from_str(&response_body).expect("parse json");
+    return serde_json::from_str(&response_body).expect("parse json");
+}
 
-    for household in reply.households {
-        println!("Household {}", household.id);
+fn get_groups(household_id: &str, access_token: &str, client_id: &str, http_client: &reqwest::Client) -> SonosGroupsReply {
+    let mut response = http_client
+        .get(
+            &format!("https://api.ws.sonos.com/control/api/v1/households/{}/groups:1", household_id))
+        .bearer_auth(access_token)
+        .header(reqwest::header::HeaderName::from_static("x-sonos-api-key"),
+                reqwest::header::HeaderValue::from_str(&client_id).expect("header value"))
+        .send()
+        .unwrap();
+
+    let response_body = response.text().expect("response_body");
+
+    println!("{:?}",  response_body);
+
+    return serde_json::from_str(&response_body).expect("parse json");
+}
+
+#[derive(Deserialize)]
+struct SonosAlbum {
+    pub name: String,
+}
+
+#[derive(Deserialize)]
+struct SonosArtist {
+    pub name: String,
+}
+
+#[derive(Deserialize)]
+struct SonosTrack {
+    pub name: String,
+    pub album: SonosAlbum,
+    pub artist: SonosArtist,
+}
+
+#[derive(Deserialize)]
+struct SonosCurrentItem {
+    pub track: SonosTrack,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct SonosPlaybackMetadata {
+    pub current_item: SonosCurrentItem,
+}
+
+fn get_playback_state(group_id: &str, access_token: &str, client_id: &str, http_client: &reqwest::Client) -> SonosPlaybackMetadata {
+    let mut response = http_client
+        .get(
+            &format!("https://api.ws.sonos.com/control/api/v1/groups/{}/playbackMetadata", group_id))
+        .bearer_auth(access_token)
+        .header(reqwest::header::HeaderName::from_static("x-sonos-api-key"),
+                reqwest::header::HeaderValue::from_str(&client_id).expect("header value"))
+        .send()
+        .unwrap();
+
+    let response_body = response.text().expect("response_body");
+
+    println!("{:?}",  response_body);
+
+    return serde_json::from_str(&response_body).expect("parse json");
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let access_token = std::env::var("ACCESS_TOKEN").expect("must set ACCESS_TOKEN");
+    let client_id = std::env::var("CLIENT_ID").expect("must set CLIENT)ID");
+
+    let http_client = reqwest::Client::new();
+    {
+        let reply = get_households(&access_token, &client_id, &http_client);
+        for household in reply.households {
+            println!("Household {}", household.id);
+            let groups_reply = get_groups(&household.id, &access_token, &client_id, &http_client);
+            for group in groups_reply.groups {
+                println!(" - {} {}", group.name, group.id);
+                let playback = get_playback_state(&group.id, &access_token, &client_id, &http_client);
+                println!("   {} {}", playback.current_item.track.name, playback.current_item.track.artist.name);
+            }
+        }
     }
 }
