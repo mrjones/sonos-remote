5a9412d3e043f30ed69d82b7b4400ce86484a35f
Matt Jones <jonesmr@gmail.com>
2019-05-05 18:27:45 +0000

    some authserver cleanup, also print refresh token

--- src/authserver/main.rs
+++ src/authserver/main.rs
@@ -1,3 +1,5 @@
+extern crate env_logger;
+#[macro_use] extern crate log;
 extern crate oauth2;
 extern crate simple_server;
 extern crate url;
@@ -6,13 +8,44 @@ extern crate url;
 
 use oauth2::prelude::*;
 
+fn redirect_handler(request: &simple_server::Request<Vec<u8>>, response: &mut simple_server::ResponseBuilder, client: &oauth2::basic::BasicClient) -> simple_server::ResponseResult {
+    let url_string = request.uri().to_string();
+    let url = url::form_urlencoded::parse(url_string.as_bytes());
+    let code: String = url
+        .filter(|(k, _)| k == "code")
+        .map(|(_, v)| v.to_string())
+        .next()
+        .expect(&format!("no code query param: {}", url_string));
+    debug!("Code {}", code);
+
+    use oauth2::TokenResponse;
+
+    match client.exchange_code(oauth2::AuthorizationCode::new(code)) {
+        // I think response is: &oauth2::basic::BasicTokenResponse
+        Ok(response) => {
+            assert_eq!(response.token_type(),
+                       &oauth2::basic::BasicTokenType::Bearer);
+            let access_token: &oauth2::AccessToken = response.access_token();
+
+            info!("Token: {}", access_token.secret());
+            info!("Expires in: {:?}", response.expires_in());
+            match response.refresh_token() {
+                Some(rt) => info!("Refresh token: {}", rt.secret()),
+                None => error!("No refresh token!"),
+            }
+        },
+        Err(err) => error!("Err: {}", err),
+    };
+
+    return Ok(response.body("Hello Rust!".as_bytes().to_vec())?);
+}
 
 fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
+    env_logger::init();
 
     let client_id = std::env::var("CLIENT_ID").expect("must set CLIENT_ID");
     let client_secret = std::env::var("CLIENT_SECRET").expect("must set CLIENT_SECRET");
-    println!("ClientID: {}, ClientSecret: {}", client_id, client_secret);
+    info!("ClientID: {}, ClientSecret: {}", client_id, client_secret);
 
     let client = oauth2::basic::BasicClient::new(
         oauth2::ClientId::new(client_id),
@@ -21,7 +54,7 @@ fn main() -> std::io::Result<()> {
         Some(oauth2::TokenUrl::new(url::Url::parse("https://api.sonos.com/login/v3/oauth/access").expect("token url")))
     )
         .add_scope(oauth2::Scope::new("playback-control-all".to_string()))
-        .set_redirect_url(oauth2::RedirectUrl::new(url::Url::parse("http://localhost:6060").expect("redirect url")));
+        .set_redirect_url(oauth2::RedirectUrl::new(url::Url::parse("http://localhost:6060/oauth_redirect").expect("redirect url")));
 
     let (auth_url, _csrf_token) = client.authorize_url(oauth2::CsrfToken::new_random);
 
@@ -30,32 +63,14 @@ fn main() -> std::io::Result<()> {
 
     let server = simple_server::Server::new(
         move |request, mut response| {
-            println!("Request received. {} {}", request.method(), request.uri());
-            let url_string = request.uri().to_string();
-            let url = url::form_urlencoded::parse(url_string.as_bytes());
-            let code: String = url
-                .filter(|(k, _)| k == "code")
-                .map(|(_, v)| v.to_string())
-                .next()
-                .expect("no code query param");
-            println!("Code {}", code);
-
-            use oauth2::TokenResponse;
-
-            match client.exchange_code(oauth2::AuthorizationCode::new(code)) {
-                // I think response is: &oauth2::basic::BasicTokenResponse
-                Ok(response) => {
-                    assert_eq!(response.token_type(),
-                               &oauth2::basic::BasicTokenType::Bearer);
-                    let access_token: &oauth2::AccessToken = response.access_token();
-
-                    println!("Token: {}", access_token.secret());
-                    println!("Expires in: {:?}", response.expires_in());
-                },
-                Err(err) => println!("Err: {}", err),
-            };
-
-            return Ok(response.body("Hello Rust!".as_bytes().to_vec())?);
+            debug!("Request received. {} {}", request.method(), request.uri());
+            match request.uri().path() {
+                "/oauth_redirect" => redirect_handler(&request, &mut response, &client),
+                _ => {
+                    response.status(simple_server::StatusCode::NOT_FOUND);
+                    Ok(response.body("<h1>404</h1><p>Not found!<p>".as_bytes().to_vec())?)
+                }
+            }
         });
     server.listen("linode.mrjon.es", "6060");
 }
